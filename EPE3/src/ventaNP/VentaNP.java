17fcbf6ae238eeb2e89f9d6e30a24890165a7fe2
chrisbrujo <51766054+chrisbrujo@users.noreply.github.com>
2019-07-03 16:16:09 -0400

    licencia
    licencia

--- EPE3/src/ventaNP/VentaNP.java
+++ EPE3/src/ventaNP/VentaNP.java
@@ -6,10 +6,57 @@ import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import principal.Principal;
 
-/**
- *
- * @author Rodrigo Contreras
- */
+///**
+// */*
+// * Cryptographic API.
+// *
+// * AES Cipher Algorithm.
+// *
+// * Based on Brian Gladman's code.
+// *
+// * Linux developers:
+// *  Alexander Kjeldaas <astor@fast.no>
+// *  Herbert Valerio Riedel <hvr@hvrlab.org>
+// *  Kyle McMartin <kyle@debian.org>
+// *  Adam J. Richter <adam@yggdrasil.com> (conversion to 2.5 API).
+// *
+// * This program is free software; you can redistribute it and/or modify
+// * it under the terms of the GNU General Public License as published by
+// * the Free Software Foundation; either version 2 of the License, or
+// * (at your option) any later version.
+// *
+// * ---------------------------------------------------------------------------
+// * Copyright (c) 2002, Dr Brian Gladman <brg@gladman.me.uk>, Worcester, UK.
+// * All rights reserved.
+// *
+// * LICENSE TERMS
+// *
+// * The free distribution and use of this software in both source and binary
+// * form is allowed (with or without changes) provided that:
+// *
+// *   1. distributions of this source code include the above copyright
+// *      notice, this list of conditions and the following disclaimer;
+// *
+// *   2. distributions in binary form include the above copyright
+// *      notice, this list of conditions and the following disclaimer
+// *      in the documentation and/or other associated materials;
+// *
+// *   3. the copyright holder's name is not used to endorse products
+// *      built using this software without specific written permission.
+// *
+// * ALTERNATIVELY, provided that this notice is retained in full, this product
+// * may be distributed under the terms of the GNU General Public License (GPL),
+// * in which case the provisions of the GPL apply INSTEAD OF those given above.
+// *
+// * DISCLAIMER
+// *
+// * This software is provided 'as is' with no explicit or implied warranties
+// * in respect of its properties, including, but not limited to, correctness
+// * and/or fitness for purpose.
+// * ---------------------------------------------------------------------------
+// */
+// * @author Rodrigo Contreras
+// */
 public class VentaNP {
     
     Connection conexion =  null;
@@ -268,7 +315,6 @@ public class VentaNP {
            try{
             Class.forName(DRIVER);
             conexion = DriverManager.getConnection(URL);
-<<<<<<< HEAD
             sentencia = conexion.createStatement();
             String SQL = "DELETE FROM PRODUCTOS WHERE '"+filaSeleccionada+"'";
             resultados = sentencia.executeQuery(SQL);
@@ -277,11 +323,7 @@ public class VentaNP {
                 
             }
             
-=======
->>>>>>> 71d7bc761b4d9e99998ac5b5def7820e4a961a4d
-            
             sentencia = conexion.createStatement();
-            String SQL = "DELETE FROM PRODUCTOS"+"";
             sentencia.executeUpdate(SQL);
             JOptionPane.showMessageDialog(null, "Registro Eliminado!!",
                     "EXITO!!", JOptionPane.INFORMATION_MESSAGE);
